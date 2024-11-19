d0e658383d8ee97b26fad2dfc8d4319ea0ada424
Abhishek Dadwal <73817744+Abhishek00810@users.noreply.github.com>
2024-11-19 12:08:29 +0530

    Update address.js

--- dist/address.js
+++ dist/address.js
@@ -1,10 +1,1 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PRIVATE_KEY_C1 = exports.TOKEN_MINT_ADDRESS = exports.PUBLIC_KEY = exports.PRIVATE_KEY = void 0;
-exports.PRIVATE_KEY = "3QLigqK4NRahktqNZTcQatBGckYnW5r5G1Kw2samH8wsrF4CS5g6n4wwZUfcDVT7NMKu994fT5f8ntftvhJxnhEg";
-exports.PUBLIC_KEY = "CKfFbJLf9bzNHESELpMHYnpKWCdL3uCXwGSz1HnmnggQ";
-exports.TOKEN_MINT_ADDRESS = "NLxKNd8mziEKn9hveoDQEzHihW36J5ir7GEMFQhrXTi";
-exports.PRIVATE_KEY_C1 = "aEAZFdqcd79U32u5f81EqkNaDDERrApW8437fsw7u38jaNVSgbt5RN12ZCXSEQV93Y3zk4wLXXvRgstVhWnfUnG";
 
-
-const URL = "https://devnet.helius-rpc.com/?api-key=800cce9d-a272-498e-bd5f-b1a1bf3c8294";
